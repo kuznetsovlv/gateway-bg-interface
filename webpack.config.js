22db086fff4881dccf0480dc8127f64598a18194
Leonid Kuznetsov <kuznetsovlv1981@gmail.com>
2023-03-27 01:09:05 +0300

    Fixed configuration to public

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,27 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const development = process.env.NODE_ENV === 'development';
 const production = !development;
 
+const output = () => {
+  const base = {
+    filename: 'index.js',
+    path: resolve(__dirname, 'dist')
+  };
+
+  if (production) {
+    base.library = 'gateway-bg-interface';
+    base.libraryTarget = 'umd';
+    base.globalObject = 'this';
+    base.umdNamedDefine = true;
+  }
+
+  return base;
+};
+
 module.exports = {
   mode: development ? 'development' : 'production',
   context: resolve(__dirname, 'src'),
   entry: production ? './index.js' : './test/index.js',
-  output: {
-    filename: 'index.js',
-    path: resolve(__dirname, 'dist')
-  },
+  output: output(),
   resolve: {
     extensions: ['.js'],
     alias: { '/': resolve(__dirname, 'src') }
